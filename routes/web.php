eaa27b72cfb51b58a35f00ea6a35f987525c34b0
vicSystems <victorasuquob@gmail.com>
2021-07-12 16:56:32 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home',[
-        'name' => 'victor'
-    ]);
+    redirect ('/login');
 });
 
 Route::get('/my_works', function () {
