5413f8fe4d4c81f0cb341fbb9cb3c7c348d490cc
vicSystems <victorasuquob@gmail.com>
2021-10-06 08:38:35 +0100

    3 modules completed

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -12,7 +12,124 @@
     <!-- END Quick Menu -->
 
     <!-- Page Content -->
+
     <div class="content">
+        <div class="d-md-flex justify-content-md-between align-items-md-center py-3 pt-md-3 pb-md-0 text-center text-md-left">
+            <div>
+                <h1 class="h2 mb-1">
+                    Dashboard
+                </h1>
+                <p class="mb-0">
+                    Welcome, admin! You have <a class="font-w500" href="javascript:void(0)">5 new notifications</a>.
+                </p>
+            </div>
+            <div class="mt-4 mt-md-0">
+                <a class="btn btn-sm btn-alt-primary" href="javascript:void(0)">
+                    <i class="fa fa-cog"></i>
+                </a>
+                <div class="dropdown d-inline-block">
+                    <button type="button" class="btn btn-sm btn-alt-primary px-3" id="dropdown-analytics-overview" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Last 30 days <i class="fa fa-fw fa-angle-down"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right font-size-sm" aria-labelledby="dropdown-analytics-overview">
+                        <a class="dropdown-item" href="javascript:void(0)">This Week</a>
+                        <a class="dropdown-item" href="javascript:void(0)">Previous Week</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="javascript:void(0)">This Month</a>
+                        <a class="dropdown-item" href="javascript:void(0)">Previous Month</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+
+        <div class="row row-deck">
+            <div class="col-sm-6 col-xl-3">
+                <div class="block block-rounded text-center d-flex flex-column">
+                    <div class="block-content block-content-full flex-grow-1">
+                        <div class="item rounded-lg bg-body-dark mx-auto my-3">
+                            <i class="fa fa-users text-muted"></i>
+                        </div>
+                        <div class="text-black font-size-h1 font-w700">2,388</div>
+                        <div class="text-muted mb-3">Registered Users</div>
+                        <div class="d-inline-block px-3 py-1 rounded-lg font-size-sm font-w600 text-success bg-success-lighter">
+                            <i class="fa fa-caret-up mr-1"></i>
+                            19.2%
+                        </div>
+                    </div>
+                    <div class="block-content block-content-full block-content-sm bg-body-light font-size-sm">
+                        <a class="font-w500" href="javascript:void(0)">
+                            View all users
+                            <i class="fa fa-arrow-right ml-1 opacity-25"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="block block-rounded text-center d-flex flex-column">
+                    <div class="block-content block-content-full flex-grow-1">
+                        <div class="item rounded-lg bg-body-dark mx-auto my-3">
+                            <i class="fa fa-level-up-alt text-muted"></i>
+                        </div>
+                        <div class="text-black font-size-h1 font-w700">14.6%</div>
+                        <div class="text-muted mb-3">Bounce Rate</div>
+                        <div class="d-inline-block px-3 py-1 rounded-lg font-size-sm font-w600 text-danger bg-danger-lighter">
+                            <i class="fa fa-caret-down mr-1"></i>
+                            2.3%
+                        </div>
+                    </div>
+                    <div class="block-content block-content-full block-content-sm bg-body-light font-size-sm">
+                        <a class="font-w500" href="javascript:void(0)">
+                            Explore analytics
+                            <i class="fa fa-arrow-right ml-1 opacity-25"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="block block-rounded text-center d-flex flex-column">
+                    <div class="block-content block-content-full flex-grow-1">
+                        <div class="item rounded-lg bg-body-dark mx-auto my-3">
+                            <i class="fa fa-chart-line text-muted"></i>
+                        </div>
+                        <div class="text-black font-size-h1 font-w700">386</div>
+                        <div class="text-muted mb-3">Confirmed Sales</div>
+                        <div class="d-inline-block px-3 py-1 rounded-lg font-size-sm font-w600 text-success bg-success-lighter">
+                            <i class="fa fa-caret-up mr-1"></i>
+                            7.9%
+                        </div>
+                    </div>
+                    <div class="block-content block-content-full block-content-sm bg-body-light font-size-sm">
+                        <a class="font-w500" href="javascript:void(0)">
+                            View all sales
+                            <i class="fa fa-arrow-right ml-1 opacity-25"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="block block-rounded text-center d-flex flex-column">
+                    <div class="block-content block-content-full">
+                        <div class="item rounded-lg bg-body-dark mx-auto my-3">
+                            <i class="fa fa-wallet text-muted"></i>
+                        </div>
+                        <div class="text-black font-size-h1 font-w700">$4,920</div>
+                        <div class="text-muted mb-3">Total Earnings</div>
+                        <div class="d-inline-block px-3 py-1 rounded-lg font-size-sm font-w600 text-danger bg-danger-lighter">
+                            <i class="fa fa-caret-down mr-1"></i>
+                            0.3%
+                        </div>
+                    </div>
+                    <div class="block-content block-content-full block-content-sm bg-body-light font-size-sm">
+                        <a class="font-w500" href="javascript:void(0)">
+                            Withdrawal options
+                            <i class="fa fa-arrow-right ml-1 opacity-25"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
        
      
     </div>
--- resources/views/general/project.blade.php
+++ resources/views/general/project.blade.php
@@ -19,31 +19,72 @@
 
 
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-12">
 
         <div class="row">
             <div class="col-3">
               <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Stage 1</a>
+
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Stage 2</a>
+                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Stage 3</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Stage 4</a>
               </div>
             </div>
             <div class="col-9">
               <div class="tab-content" id="v-pills-tabContent">
-                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                    
+                    
+                <div class="card">
+                    <div class="card-body py-5">
+                        <a href="" class="btn btn-primary">Create Report</a>
+                    </div>
+                </div>
+                
+                
+                </div>
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                    
+                    
+                <div class="card">
+                    <div class="card-body py-5">
+                        <a href="" class="btn btn-primary">Create Report</a>
+                    </div>
+                </div>
+                
+                
+                </div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                    
+                    
+                <div class="card">
+                    <div class="card-body py-5">
+                        <a href="" class="btn btn-primary">Create Report</a>
+                    </div>
+                </div>
+                
+                
+                </div>
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                    
+                    
+                <div class="card">
+                    <div class="card-body py-5">
+                        <a href="" class="btn btn-primary">Create Report</a>
+                    </div>
+                </div>
+                
+                
+                </div>
               </div>
             </div>
           </div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-12">
         <h4>Correspondence</h4>
 
-        
+
 
     </div>
 
